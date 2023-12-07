4f4f5455ec0cfcecbece504824e1cde2a11f8173
et <ethantse42@gmail.com>
2023-12-07 11:31:10 -0500

    update model to correctly specify normal and reference populations to have 0 variant alleles

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ Script for hierarchical bayesian modelling
 @author: Ethan Tse 
 """
 
+from statistics import mode
 import pyro
 import pyro.distributions as dist
 import torch
@@ -16,6 +17,7 @@ from pyro.distributions.constraints import positive
 from pyro.infer import SVI, TraceEnum_ELBO, config_enumerate, infer_discrete
 from pyro.optim import Adam
 from pyro.infer.autoguide import AutoNormal
+from pyro.infer.autoguide.initialization import init_to_mean, init_to_sample
 
 # LKJcholesky prior for covariance matrix
 # y ~ MVN
@@ -42,22 +44,6 @@ def probability_function(tumour_content, variant_allele_count, copy_number_state
     # variant_allele_vector = [0, 0, variant_allele_count]
     variant_allele_vector = variant_allele_count
 
-    # # Pyclone's function
-    # c1 = (1-tumour_content)*copy_number_vector[0]
-    # c2 = tumour_content*(1-cancer_cell_fraction)*copy_number_vector[1]
-    # c3 = tumour_content*cancer_cell_fraction*copy_number_vector[2]
-
-    # z_constant = (1-tumour_content)*copy_number_vector[0] + \
-    #     tumour_content*(1-cancer_cell_fraction)*copy_number_vector[1] + \
-    #     tumour_content*cancer_cell_fraction*copy_number_vector[2]
-
-    # normal_pop = (((1-tumour_content)*copy_number_vector[0])/z_constant) * \
-    #     (variant_allele_vector[0]/copy_number_vector[0])
-    # reference_pop = ((tumour_content*(1-cancer_cell_fraction)*copy_number_vector[1])/z_constant) * \
-    #     (variant_allele_vector[1]/copy_number_vector[1])
-    # variant_pop = ((tumour_content*cancer_cell_fraction*copy_number_vector[2])/z_constant) * \
-    #     (variant_allele_vector[2]/copy_number_vector[2])
-
     # Pyclone's function
     c1 = (1-tumour_content)*copy_number_vector
     c2 = tumour_content*(1-cancer_cell_fraction)*copy_number_vector
@@ -68,14 +54,14 @@ def probability_function(tumour_content, variant_allele_count, copy_number_state
         tumour_content*cancer_cell_fraction*copy_number_vector
 
     normal_pop = (((1-tumour_content)*copy_number_vector)/z_constant) * \
-        (variant_allele_vector/copy_number_vector)
+        (variant_allele_vector[0]/copy_number_vector)
     reference_pop = ((tumour_content*(1-cancer_cell_fraction)*copy_number_vector)/z_constant) * \
-        (variant_allele_vector/copy_number_vector)
+        (variant_allele_vector[1]/copy_number_vector)
     variant_pop = ((tumour_content*cancer_cell_fraction*copy_number_vector)/z_constant) * \
-        (variant_allele_vector/copy_number_vector)
+        (variant_allele_vector[2]/copy_number_vector)
 
     probability = normal_pop + reference_pop + variant_pop
-
+    
     return probability
 
 
@@ -111,12 +97,12 @@ def model(data):
 
     # torch.ones(K) is an uniformative dirichlet prior (we put no emphasis on any possibility.)
     mixture_coefficients = pyro.sample(
-        "pi", dist.Dirichlet(dirichlet_alpha))
+        "mixture_coef", dist.Dirichlet(dirichlet_alpha))
 
     # we do not assume clusters have different SDs
     # LKJ params: d, concentration, where d is the number of clusters
     cluster_cov = pyro.sample(
-        "cov", dist.InverseGamma(1, 1))
+        "sd", dist.InverseGamma(1, 1))
 
     # -------------------------------------------------------------------------------------------
 
@@ -124,7 +110,9 @@ def model(data):
     with pyro.plate("k", K):
         # prior on cluster means
         cluster_means = pyro.sample(
-            "mu", dist.Normal(torch.tensor([0.0]), torch.tensor([1.0])))
+            "mu", dist.Normal(0, 1))
+        
+        cluster_means = torch.special.expit(cluster_means)
 
     # -------------------------------------------------------------------------------------------
 
@@ -146,7 +134,7 @@ def model(data):
 
         # these are what we infer: what cluster does a mutation belong to? what is its CCF?
         cluster_assignment = pyro.sample(
-            "h", dist.Categorical(mixture_coefficients))
+            "cluster_assignment", dist.Categorical(mixture_coefficients))
 
         # our CCF is modelled with a gaussian mixture model:
         cancer_cell_fraction = pyro.sample(
@@ -175,14 +163,17 @@ def model(data):
         # Read information ----------------------
         # num supporting reads follows a binomial distribution, with number of tries = number of total reads covered
         num_supporting_reads = pyro.sample(
-            "r_n", fn=dist.Binomial(num_total_reads, probability_function(tumour_content, variant_allele_count, copy_number_state, cancer_cell_fraction)), obs=num_supporting_reads)
+            "supporting_reads", fn=dist.Binomial(num_total_reads, probability_function(tumour_content, variant_allele_count, copy_number_state, cancer_cell_fraction)), obs=num_supporting_reads)
+
+        # num_supporting_reads = pyro.sample(
+        #     "supporting_reads", fn=dist.Binomial(num_total_reads, cancer_cell_fraction), obs=num_supporting_reads)
 
     return cluster_assignment, cancer_cell_fraction
 
 
 def train_gmm(data, num_steps=10000):
-    optimizer = Adam(optim_args={"lr": 0.001})  # decrease learning rate
-    guide = AutoNormal(poutine.block(model, hide=["h"]))
+    optimizer = Adam(optim_args={"lr": 0.05})  # decrease learning rate
+    autoguide = AutoNormal(poutine.block(model, hide=["cluster_assignment"]), init_loc_fn=init_to_sample)
 
     trace = poutine.trace(model).get_trace(data)
     trace.compute_log_prob()
@@ -191,7 +182,7 @@ def train_gmm(data, num_steps=10000):
 
     print(trace.log_prob_sum())
 
-    svi = SVI(model, guide, optimizer, loss=TraceEnum_ELBO())
+    svi = SVI(model, autoguide, optimizer, loss=TraceEnum_ELBO())
 
     # save losses
     losses = []
@@ -200,7 +191,26 @@ def train_gmm(data, num_steps=10000):
         losses.append(loss)
         if step % 1000 == 0 or step == num_steps:
             print(f"Step {step}/{num_steps}, Loss: {loss}")
-    return losses
+            
+    # grab the learned variational parameters
+    # Get the keys of parameters in the parameter store
+    param_keys = pyro.get_param_store().keys()
+
+    # Print the keys
+    print("Parameter Keys:", param_keys)
+    
+    mu_loc = pyro.param("AutoNormal.locs.mu")
+    print(torch.special.expit((mu_loc)))
+    
+    ccf_loc = pyro.param("AutoNormal.locs.CCF")
+    print(torch.special.expit((ccf_loc)))
+    
+    ccf_scales = pyro.param("AutoNormal.scales.CCF")
+    print(ccf_scales)
+    
+    print(f"CCF values are: {torch.special.expit((ccf_loc)).detach().numpy()}")
+    
+    return losses, torch.special.expit((ccf_loc)).detach().numpy(), torch.special.expit((mu_loc)).detach().numpy()
 
 
 def classifier(data, trained_model, temperature=0):
@@ -211,7 +221,7 @@ def classifier(data, trained_model, temperature=0):
     trace = pyro.poutine.trace(inferred_model).get_trace(data)
 
     ccf = trace.nodes["CCF"]["value"]
-    cluster = trace.nodes["h"]["value"]
+    cluster = trace.nodes["cluster_assignment"]["value"]
 
     result = {"ccf": ccf,
               "cluster": cluster
@@ -225,7 +235,6 @@ def plot_training_error(loss, num_steps):
     plt.xlabel('Steps')
     plt.ylabel('Loss')
     plt.title('SVI Training Loss')
-    plt.show()
 
     plt.savefig(f"./code_outputs/model/loss_{num_steps}.png")
 
@@ -237,11 +246,15 @@ def process_outputs(ccf, clusters):
 
 
 def main():
-    K = 10  # K clusters
-
+    pyro.util.set_rng_seed(12345)
+    # K clusters
+    K = 10
     # -------------------------
-    df = pd.read_csv("./somatic_svs.tsv",
+    df = pd.read_csv("./COLO829_somatic_forced_synthetic.tsv",
                      delimiter="\t", index_col=None)
+    
+    # df = pd.read_csv("./somatic_svs.tsv",
+    #                  delimiter="\t", index_col=None)
 
     # Get params:
     num_svs = df.shape[0]  # get number of rows == number of svs
@@ -255,6 +268,8 @@ def main():
     # Genotype Information
     # genotype = df["genotype"].to_numpy()
     variant_allele_count = df["variant_allele_count"].to_numpy()
+    variant_allele_count = [variant_allele_count*0, variant_allele_count*0, variant_allele_count]
+    
     # we assume all are diploid to start
     copy_number_state = df["copy_number"].to_numpy()
 
@@ -274,39 +289,57 @@ def main():
         "tumour_content": torch.tensor(tumour_content)
     }
 
-    # ------------------------- Render model
-    # pyro.render_model(model,
-    #                   model_args=data,
-    #                   filename="./code_outputs/model/model.png",
-    #                   render_distributions=True)
+    # ------------------------------ Run Model
+    num_steps = 10000
 
-    # ------------------------- Run Model
-    num_steps = 100
-
-    losses = train_gmm(data, num_steps=num_steps)
+    losses, ccfs, cluster_means = train_gmm(data, num_steps=num_steps)
+    
+    # Save inferred values
+    df["ccf"] = ccfs
 
     plot_training_error(loss=losses, num_steps=num_steps)
 
+    # ------------------------------ Render Model
     pyro.render_model(model, model_args=(data,),
-                      filename="./code_outputs/model/model.png")
+                      filename="./code_outputs/model/model.png", 
+                      render_params=True)
 
     print("Finish")
 
+    # ------------------------------ After SVI, we want to get the estimates (e.g. mean of the posterior)
     # Get information:
     autoguide = AutoNormal(pyro.poutine.block(
-        model, hide=['h']))
+        model, hide=['cluster_assignment']))
     guide_trace = pyro.poutine.trace(autoguide).get_trace(
         data)  # record the globals
     trained_model = pyro.poutine.replay(
         model, trace=guide_trace)  # replay the globals
+    
+    num_samples = 1000
+    ccf_samples = []
+    cluster_assignment_samples = []
+    for sample in range(num_samples):
+        guide_trace = pyro.poutine.trace(autoguide).get_trace(data)
+        posterior_predictive = pyro.poutine.trace(pyro.poutine.replay(model, guide_trace)).get_trace(data)
+        ccf_samples.append(torch.special.expit(posterior_predictive.nodes['CCF']['value']).detach().numpy())   
+        cluster_assignment_samples.append(posterior_predictive.nodes['cluster_assignment']['value'].detach().numpy())    
 
-    test_values = classifier(data, trained_model=trained_model)
+    ccf_sampling_df = pd.DataFrame(ccf_samples)
+    cluster_assignment_sampling_df = pd.DataFrame(cluster_assignment_samples)
+    
+    # Calculate the mode for each column
+    cluster_id_list = cluster_assignment_sampling_df.mode().iloc[0].tolist()
+    cluster_id_list = list(map(int, cluster_id_list))
+    
+    print(cluster_means)
+    # Save inferred values
+    df["cluster_id"] = cluster_id_list
+    df['cluster_mean'] = df['cluster_id'].map(lambda x: cluster_means[x])
+    
+    # Writing to a CSV file
+    df.to_csv('./code_outputs/model/tsv/df_with_ccf.tsv', index=False, sep="\t")
 
-    cluster_assignments = test_values["cluster"].to_numpy()
-    ccfs = test_values["ccf"].to_numpy()
-
-    process_outputs(ccf=ccfs, clusters=cluster_assignments)
-
+    print("hello")
 
 if __name__ == "__main__":
     main()
